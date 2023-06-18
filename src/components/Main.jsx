5f385fc1440f41032f00f83f36377cff106747b8
Rbarriosbm <rjbarrios@uninorte.edu.co>
2023-06-18 02:06:29 -0500

    feat: first hello world

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import MainstyleCSS from '../stiles/MainStyle.module.css'
+
+function Main() {
+    return <>
+        <div>
+            <h1 className={MainstyleCSS.title}>HolaMundo</h1>
+        </div>
+    </>
+}
+
+export default Main
