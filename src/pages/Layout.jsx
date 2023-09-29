8b873e3b67758a616a9b01ebf1b64d68a2d0f397
RickBm400 <rjbm29@gmail.com>
2023-09-29 14:56:44 -0500

    style: disable guts in layout

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -4,10 +4,14 @@ import { Outlet } from 'react-router-dom'
 export default function Home() {
     return (
         <Container maxWidth='100vh'
+        disableGutters
             sx={{
-                bgcolor: '#303433',
+                display: 'flex',
+                alignItems: 'center',
+                flexDirection: 'column',
                 minHeight: '100vh',
-                paddingTop: '20px'
+                paddingTop: '20px',
+                paddingX: {xs: '0px', md: '70px'}
             }}>
             <NavBar />
             <Outlet/>
